806e999ba04b2f1d405cc490c97b43341d3cfcf8
Danny Incarnato <>
2026-06-24 23:49:15 +0200

    v1.1.1

--- main.js
+++ main.js
@@ -54,7 +54,7 @@ if (isCLI) {
 if (cliArgs.help || cliArgs.h) {
 
     console.log(`
- RFview (v1.1.0)
+ RFview (v1.1.1)
  RNA Framework Structure Viewer [https://github.com/dincarnato/RFviewJS]
 
  Author:   Danny Incarnato (dincarnato[at]rnaframework.com)
